5f871f9fb777db8f57703747885645e610214494
stvnu <stevenyuuuu@gmail.com>
2024-07-25 21:59:03 +0800

    Update index.js

--- ProjectFolder/index.js
+++ ProjectFolder/index.js
@@ -71,7 +71,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
-app.use(express.static('public'));
+app.use("/public", express.static('public'));
 app.use("/uploads", express.static('uploads'));
 
 app.use(session({
