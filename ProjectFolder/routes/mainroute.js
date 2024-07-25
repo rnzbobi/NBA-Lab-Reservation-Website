51f2f345c49035d0e725e717e3d45f12bc1cdfe0
stvnu <stevenyuuuu@gmail.com>
2024-07-25 20:26:24 +0800

    Test FIX for Register

--- ProjectFolder/routes/mainroute.js
+++ ProjectFolder/routes/mainroute.js
@@ -43,15 +43,21 @@ const getReservedSeatsForTimeslot = async (reservationStart, reservationEnd, sta
 };
 
 // Setup multer for file handling
+const fs = require('fs');
 const storage = multer.diskStorage({
   destination: function (req, file, cb) {
-    cb(null, 'public/profilepics');  // Ensure this directory exists
+    const dir = 'public/profilepics';
+    if (!fs.existsSync(dir)){
+      fs.mkdirSync(dir, { recursive: true });
+    }
+    cb(null, dir);
   },
   filename: function (req, file, cb) {
     const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1E9);
     cb(null, file.fieldname + '-' + uniqueSuffix + path.extname(file.originalname));
   }
 });
+
 
 const upload = multer({ storage: storage });
 
